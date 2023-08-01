ef04cad36a9ba2f7dfd81c279304a25f2e185d3b
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-08-02 00:07:12 +0545

    fix!: extra button href when filters are used

--- form_action/mixins.py
+++ form_action/mixins.py
@@ -1,13 +1,14 @@
+from urllib.parse import urljoin
+
 from django.template import engines
 from django.urls import path
 
 template = engines["django"].from_string("""
 {% extends "admin/change_list.html" %}
-{% load add_preserved_filters from admin_urls %}
 {% block object-tools-items %}
   {% for button in extra_buttons %}
     <li>
-      <a href="{% add_preserved_filters action_url %}actions/{{ button.name }}/">
+      <a href="{{ button.url }}">
         {{ button.title }}
       </a>
     </li>
@@ -23,7 +24,14 @@ class ExtraButtonMixin:
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
         extra_buttons = getattr(self, "extra_buttons", [])
-        extra_buttons = [{"name": b.name, "title": b.title} for b in extra_buttons]
+        extra_buttons = [
+            {
+                "name": b.name,
+                "title": b.title,
+                "url": urljoin(request.path, f"actions/{b.name}/"),
+            }
+            for b in extra_buttons
+        ]
         extra_context.update({"extra_buttons": extra_buttons})
         return super().changelist_view(request, extra_context)
 
